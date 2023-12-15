a8e1cf83de9d9bed2d2faf61443d5fe110027a90
Inosukex07 <konlawichthawon@gmail.com>
2023-12-15 14:21:12 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.header("Konlawich Thawon")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('นายกลิวชร ถาวร')
+st.subheader('สาขาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
