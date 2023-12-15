1f81f1cc331638829537afa1f1a93fc8df4e881e
Inosukex07 <konlawichthawon@gmail.com>
2023-12-15 14:35:37 +0700

    .commit

--- Home.py
+++ Home.py
@@ -15,7 +15,7 @@ with col2:
 
     html_1 = """
 <div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+<center><h5>สถิติข้อมูลน้องหมา</h5></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
