8ddb273d15a8adc52b8c58f86c8f19c9ee5c4ab2
Inosukex07 <konlawichthawon@gmail.com>
2023-12-15 14:27:35 +0700

    .commit

--- Home.py
+++ Home.py
@@ -4,3 +4,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('นายกลิวชร ถาวร')
 st.subheader('สาขาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/Profile.jpg')
+with col2:
+    st.image('./pic/sunflower.jpg')
